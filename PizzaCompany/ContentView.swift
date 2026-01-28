5c41475f725776b954d5d95c1a26a0c89706f8a2
Bachapally sai vardhan reddy <bachapallysaivardhan.reddy@amadeus.com>
2026-01-28 18:42:18 +0530

    Initial Commit

---
+++ PizzaCompany/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizzaCompany
+//
+//  Created by Bachapally Sai Vardhan REDDY on 28/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PizzaCompany/PizzaCompanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaCompanyApp.swift
+//  PizzaCompany
+//
+//  Created by Bachapally Sai Vardhan REDDY on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PizzaCompanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
